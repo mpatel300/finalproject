ee47f89392fbe4df268f74623005b156cbae0d7e
mpatel300 <manavpatel01@gmail.com>
2024-12-07 05:03:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,28 @@ import streamlit as st
 # Title of the page
 st.title("YouTube Statistics")
 
-# This shows the websites desc. along with a message.
-st.write("This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
-
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to section:", ["Global Viewership Data", "Section 2", "Section 3", "Section 4"])
 
 if page == "Global Viewership Data":
+    st.write("This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
     st.image("Map.png", caption="Global Viewership Data (World-Wide)", use_column_width=True)
     st.write("This is the World Global Viewship Data, in this data you can find the following ...")
+
+elif page == "Section 2":
+    st.image("https://picsum.photos/800/401", caption="describe the photo by text here", use_column_width=True)
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.222222222222
+    """)
+
+elif page == "Section 3":
+    st.image("https://picsum.photos/800/402", caption="describe the photo by text here", use_column_width=True)
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.33333333333
+    """)
+
+elif page == "Section 4":
+    st.image("https://picsum.photos/800/403", caption="describe the photo by text here", use_column_width=True)
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.444444444444444
+    """)
