66db035eeae9a74c1cc9568dc4fde110c9fbc437
mpatel300 <manavpatel01@gmail.com>
2024-12-07 13:18:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,17 @@ st.title(":red[YouTube] Statistics")
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to section:", ["Global Viewership Data", "Section 2", "Section 3", "Section 4"])
 
-if page == "Overview":
-    st.write("Welcome to my app, your ultimate companion for YouTube growth and insights! This app is designed to empower content creators by providing in-depth analytics and actionable insights. Whether you're just starting your YouTube journey or looking to take your channel to the next level.")
-    
-elif page == "Global Viewership Data":
+if page == "Global Viewership Data":
+    st.write("**This** website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
     st.write("This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
     st.image("Map.png", caption="Global Viewership Data (World-Wide)", use_column_width=True)
     st.write("This is the World Global Viewship Data, in this data you can find the following ...")
 
-
 elif page == "Categories By the Popularity":
     st.image("https://picsum.photos/800/401", caption="describe the photo by text here", use_column_width=True)
-    st.write("")
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.222222222222
+    """)
 
 elif page == "Section 3":
     st.image("https://picsum.photos/800/402", caption="describe the photo by text here", use_column_width=True)
