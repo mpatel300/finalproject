c5bd4e8a83dbfcdaee9f5e588a11eb31cd1289b7
mpatel300 <manavpatel01@gmail.com>
2024-12-07 05:20:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to section:", ["Global Viewership Data", "Section 2", "Section 3", "Section 4"])
 
 if page == "Global Viewership Data":
-    st.write("**This** website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
+    st.write("This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
     st.image("Map.png", caption="Global Viewership Data (World-Wide)", use_column_width=True)
     st.write("This is the World Global Viewship Data, in this data you can find the following ...")
 
