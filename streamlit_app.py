ebd464cdae537376569b395638cec503f8bbd4fd
mpatel300 <manavpatel01@gmail.com>
2024-12-07 14:03:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,11 @@ st.title(":red[YouTube] Statistics")
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to section:", ["Test", "Global Viewership Data", "Categories By the Popularity", "Viewership to Subscriber Ratio by Country", "Section 4"])
 
-if page == "Global Viewership Data":
+if page == "Test":
+    st.write("Welcome to my app, your ultimate companion for YouTube growth and insights! This app is designed to empower content creators by providing in-depth analytics and actionable insights. Whether you're just starting your YouTube journey or looking to take your channel to the next level.")
+
+
+elif page == "Global Viewership Data":
     st.write("This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
     st.image("Map.png", caption="Global Viewership Data (World-Wide)", use_column_width=True)
     st.write("This is the World Global Viewship Data, in this data you can find the following ...")
