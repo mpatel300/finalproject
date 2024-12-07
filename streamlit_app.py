171e23f25325a506297b1c9d51db23e2b41dee06
mpatel300 <manavpatel01@gmail.com>
2024-12-07 05:00:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import streamlit as st
 st.title("YouTube Statistics")
 
 # This shows the websites desc. along with a message.
-st.write(" This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone
-who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
+st.write("This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
 
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to section:", ["Global Viewership Data", "Section 2", "Section 3", "Section 4"])
