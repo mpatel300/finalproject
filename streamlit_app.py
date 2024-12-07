d4a93967868c9e50554ff9fcbe7687584c9c31b7
mpatel300 <manavpatel01@gmail.com>
2024-12-07 05:19:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ page = st.sidebar.radio("Go to section:", ["Global Viewership Data", "Section 2"
 
 if page == "Global Viewership Data":
     st.write("This website is a showcase and indepth view of Youtube Stats from many different variables, the insight that can be found in the different tabs can help someone who already has a youtube channel or someone who wants to take there youtube channel to the next level.")
-    st.image("Map.png", caption="Global Viewership Data (World-Wide)", use_column_width=True)
+    st.image("Map.png", caption="**Global Viewership Data (World-Wide)**", use_column_width=True)
     st.write("This is the World Global Viewship Data, in this data you can find the following ...")
 
 elif page == "Categories By the Popularity":
