791f3398d233c7a8ef0f7098128820abcb7bb425
mpatel300 <manavpatel01@gmail.com>
2024-12-07 16:06:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ if page == "**Overview**":
 elif page == "**Global Viewership Data**":
     st.write("tab provides a comprehensive look at YouTube audience trends across the globe. This feature is perfect for creators who want to expand their reach and create content that resonates with an international audience."
 
-What You’ll Discover:
+"What You’ll Discover:"
 
-Regional Trends: See which types of content perform best in different countries and regions.
+"Regional Trends: See which types of content perform best in different countries and regions.
 Audience Insights: Understand global viewer preferences, habits, and engagement patterns.
 Growth Opportunities: Identify untapped markets and tailor your content to attract diverse audiences worldwide.
 Whether you're planning to localize your content or broaden your appeal, this tab equips you with the insights to grow your channel on a global scale.
