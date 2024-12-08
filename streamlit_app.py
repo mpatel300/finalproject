20de284b5cf5d116e0043b8a46fa117661e24dae
mpatel300 <manavpatel01@gmail.com>
2024-12-08 03:26:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,17 @@ if page == "**Overview**":
     st.write("Welcome to ultimate YouTube Statistic App, your ultimate companion for YouTube growth and insights! Unlock the power of data to supercharge your channel’s performance and creativity. This app is designed to empower content creators like you by providing in-depth analytics, actionable insights, and trend-driven recommendations. Whether you're just starting your YouTube journey or striving to take your channel to the next level, we've got everything you need to thrive in the competitive world of content creation. Get ready to transform your ideas into results, uncover hidden opportunities, and connect with your audience like never before. Your next big milestone starts here!")
     st.image("background.1.png", use_column_width=True)
 
+st.write("""
+    **Title**
+    - Point 1
+    - Point 2
+    - Point 3
+    
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. 
+    Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+""")
+
+
 elif page == "**Global Viewership Data**":
     st.write("tab provides a comprehensive look at YouTube audience trends across the globe. This feature is perfect for creators who want to expand their reach and create content that resonates with an international audience. What You’ll Discover, Regional Trends: See which types of content perform best in different countries and regions.Audience Insights: Understand global viewer preferences, habits, and engagement patterns. Growth Opportunities: Identify untapped markets and tailor your content to attract diverse audiences worldwide.Whether you're planning to localize your content or broaden your appeal, this tab equips you with the insights to grow your channel on a global scale.")
     st.image("Global Viewership Data.png", caption="Global Viewership Data (World-Wide)", use_column_width=True)
